4f37742c6a84eff24a68ac012ef84ee0a68b3728
Danny Regan <danny.regan@protonmail.com>
2020-07-08 18:02:11 +0100

    osm api lookup, all townland co-ordinates found

---
+++ config.py
@@ -0,0 +1,4 @@
+import rootpath
+ROOT_DIR = rootpath.detect()
+data_path = f"{ROOT_DIR}/data"
+population_data_path = f"{data_path}/population"
--- graph/population_helper.py
+++ graph/population_helper.py
@@ -1,12 +1,10 @@
 import pandas as pd
 from openpyxl import load_workbook
-from OSMPythonTools.api import Api
 from OSMPythonTools.nominatim import Nominatim
+import config
 
-csv_filename = '../data/population/donegal_townlands.csv'
 
-
-def extract_county_townlands_from_source_data(county_abbr='DL'):
+def extract_county_townlands_from_source_data(county_abbr="DL"):
     """
     Filter source population data to extract:
         - Donegal townland addresses
@@ -15,42 +13,39 @@ def extract_county_townlands_from_source_data(county_abbr='DL'):
     """
     # load excel data
     population_data = load_workbook(
-        '../data/population/COP2016_Townlands.xlsx')
+        "../data/population/COP2016_Townlands.xlsx")
 
     # convert to pandas dataframe, set first row as header
     df = pd.DataFrame(population_data.worksheets[0].values).T.set_index(0).T
     # filter so only Donegal townlands remain
-    df = df[df['COUNTY'] == county_abbr]
+    df = df[df["COUNTY"] == county_abbr]
     # keep only relevant data, i.e. address and 2016 census total
-    df = df[['TLANDNAME', 'EDNAMES_3409S', 'COUNTYNAME', 'TOTAL2016']]
-    df['address'] = df[['TLANDNAME',
-                        'EDNAMES_3409S',
-                        'COUNTYNAME']].agg(', '.join, axis=1)
-    df.columns = ['townland', 'town', 'county', 'population', 'address']
+    df = df[["TLANDNAME", "EDNAMES_3409S", "TOTAL2016"]]
+    df.columns = ["townland", "town", "population"]
     # remove any rows where population is -1 or 0 as these are not useful
-    df = df[df['population'] > 0]
-    df.to_csv(csv_filename)
+    df = df[df["population"] > 0]
+    df.to_csv(f"{config.population_data_path}/donegal_townlands.csv")
     return df
 
 
-def extract_lat_long_from_nominatim(townland, county):
+def extract_lat_long_from_nominatim(address):
     lat, lng = None, None
     nominatim = Nominatim()
-    area = nominatim.query(townland)
+    area = nominatim.query(address)
     if area is None:
         return None, None
     try:
-        '''
+        """
         Try block in case any inputs are invalid
-        '''
+        """
         osm_json = area.toJSON()
         json_item = None
         for item in osm_json:
-            if county in item['display_name']:
+            if "Donegal" in item["display_name"]:
                 json_item = item
                 break
-        lat = json_item['lat']
-        lng = json_item['lon']
+        lat = json_item["lat"]
+        lng = json_item["lon"]
     except:
         pass
     return lat, lng
@@ -62,14 +57,12 @@ def lookup_osm_coordinates(row, column):
     :return:
     """
     address_value = row[column]
-    county = row['county']
-    address_lat, address_lng = extract_lat_long_from_nominatim(address_value, county)
-    row['lat'] = address_lat
-    row['lng'] = address_lng
+    address_lat, address_lng = extract_lat_long_from_nominatim(address_value)
+    row["lat"] = address_lat
+    row["lng"] = address_lng
     return row
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     donegal_data = extract_county_townlands_from_source_data()
-    #donegal_data = pd.read_csv(csv_filename)
-    donegal_data = donegal_data.apply(lookup_osm_coordinates, args=('townland',), axis=1)
+    donegal_data = donegal_data.apply(lookup_osm_coordinates, args=("townland",), axis=1)
     donegal_data.to_csv(csv_filename)
