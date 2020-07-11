a5e2a13abcf8bccd39151a7fb52f7cd4cf30ccd9
Danny Regan <danny.regan@protonmail.com>
2020-07-11 17:33:41 +0100

    applying weights to graph edges

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ ROOT_DIR = rootpath.detect()
 data_path = f"{ROOT_DIR}/data"
 population_data_path = f"{data_path}/population"
 graph_path = f"{ROOT_DIR}/graph"
+graphml_path = f"{graph_path}/graphml"
 gnn_path = f"{ROOT_DIR}/gnn"
