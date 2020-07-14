ee3b0a192a44115e587b96f2f9d643bc8f35cd84
Danny Regan <danny.regan@protonmail.com>
2020-07-14 20:53:59 +0100

    started on generating training graph

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ ROOT_DIR = rootpath.detect()
 data_path = f"{ROOT_DIR}/data"
 population_data_path = f"{data_path}/population"
 graph_path = f"{ROOT_DIR}/graph"
-graphml_path = f"{graph_path}/graphml"
+graph_graphml_path = f"{graph_path}/graphml"
 gnn_path = f"{ROOT_DIR}/gnn"
+gnn_pickle_path = f"{gnn_path}/pickle"
